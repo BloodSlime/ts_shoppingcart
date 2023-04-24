bb3d294f2b41933b19419988473b940fe2173024
BloodSlime <p.shishmarov@gmail.com>
2023-04-24 10:12:35 +0300

    nobody reads it anyway

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { useQuery } from 'react-query';
-import './styles/css/App.css'
-import Cart from './Cart/Cart';
-import { useAppSelector, useAppDispatch } from './store'
-import { ADD } from './reducer';
+import { useQuery } from "react-query";
+import "./styles/css/App.css";
+import Cart from "./Cart/Cart";
+import { useAppSelector, useAppDispatch } from "./store";
+import { ADD } from "./reducer";
+import axios from "axios";
 
 type Items = {
   id: number;
@@ -10,39 +11,77 @@ type Items = {
   price: number;
   category: string;
   description: string;
-  image: string
+  image: string;
+};
+
+// const getData = async (): Promise<Items[]> => {
+// return await (await fetch("https://fakestoreapi.com/products")).json();
+// };
+
+// const data = getItems();
+
+interface Response {
+  data: Items[];
 }
 
-const getData = async ():Promise<Items[]> => {
-  return await (await fetch('https://fakestoreapi.com/products')).json()
+async function getItems() {
+  const { data: response } = await axios.get(
+    "https://fakestoreapi.com/products",
+    {
+      headers: {
+        "Accept-Encoding": "application/json",
+      },
+    }
+  );
+  console.log(response);
 }
+
+// let data;
+// axios
+// .get("https://fakestoreapi.com/products")
+// .then((response) => (data = response.data));
 
 function App() {
-  const {data, isLoading, error} = useQuery<Items[]>('products', getData)
+  // const { data, isLoading, error } = useQuery<Items[]>("products", getData);
   const dispatch = useAppDispatch();
-  console.log(data)
 
-  const addToCart = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, image: string, title: string, id: number, amount: number, cost: number) => {
-    e.preventDefault()
+  const addToCart = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    image: string,
+    title: string,
+    id: number,
+    amount: number,
+    cost: number
+  ) => {
+    e.preventDefault();
 
-    dispatch(ADD({image, title, id, amount, cost}))
-  }
-  
+    dispatch(ADD({ image, title, id, amount, cost }));
+  };
+
   return (
     <div className="App">
-      <Cart/>
-      <div className='shopping-page'>
-        {data?.map((item)=> {
-          return <div className='item' key={item.id}>
-            <div className='img-wrapper'>
-              <img src={item.image} alt={item.description}/>
+      <Cart />
+      <div className="shopping-page">
+        {data.map((item: Items) => {
+          return (
+            <div className="item" key={item.id}>
+              <div className="img-wrapper">
+                <img src={item.image} alt={item.description} />
+              </div>
+              <h3>{item.title}</h3>
+              <h3>${item.price}</h3>
+              <div>
+                <button
+                  style={{ cursor: "pointer" }}
+                  onClick={(e) =>
+                    addToCart(e, item.image, item.title, item.id, 1, item.price)
+                  }
+                >
+                  +
+                </button>
+              </div>
             </div>
-            <h3>{item.title}</h3>
-            <h3>${item.price}</h3>
-            <div>
-              <button style={{cursor: 'pointer'}} onClick={e => addToCart(e, item.image, item.title, item.id, 1, item.price)}>+</button>
-            </div>
-          </div>
+          );
         })}
       </div>
     </div>
--- src/reducer.tsx
+++ src/reducer.tsx
@@ -1,45 +1,61 @@
 import { createReducer, createAction, createSlice } from "@reduxjs/toolkit";
 
 export type Item = {
-    image: string,
-    title: string,
-    amount: number,
-    cost: number,
-    id: number
-}
+  image: string;
+  title: string;
+  amount: number;
+  cost: number;
+  id: number;
+};
 
-export type CartType ={
-    items: Array<Item>,
-    totalCost: number
-}
+export type CartType = {
+  items: Array<Item>;
+  totalCost: number;
+};
 
 type Action = {
-    payload: any
-}
+  payload: any;
+};
 
 const cartItemsSlice = createSlice({
-    name: 'cartItemsSlice',
-    initialState: {
-        items: [],
-        totalCost: 0
+  name: "cartItemsSlice",
+  initialState: {
+    items: [],
+    totalCost: 0,
+  },
+  reducers: {
+    ADD(state: CartType, action: Action) {
+      const itemInCart: Item | undefined = state.items.find(
+        (item) => action.payload.title === item.title
+      );
+      if (itemInCart) {
+        state.items[state.items.indexOf(itemInCart)].amount +=
+          action.payload.amount;
+        state.items[state.items.indexOf(itemInCart)].cost = action.payload.cost;
+        state.totalCost += action.payload.cost;
+      } else {
+        state.items.push(action.payload);
+        state.totalCost += action.payload.cost;
+      }
     },
-    reducers: {
-        ADD(state: CartType, action: Action) {
-            const itemInCart: Item | undefined = state.items.find(item => action.payload.title === item.title)
-            if (itemInCart) {
-                state.items[state.items.indexOf(itemInCart)].amount += action.payload.amount
-                state.items[state.items.indexOf(itemInCart)].cost = action.payload.cost
-                state.totalCost += action.payload.cost 
-                console.log(state.totalCost)
-            } else {
-                state.items.push(action.payload)
-                state.totalCost += action.payload.cost 
-                console.log(state.totalCost)
-            }
-            
+    DELETE(state: CartType, action: Action) {
+      const CartItem: Item | undefined = state.items.find(
+        (item) => action.payload.title === item.title
+      );
+      if (CartItem) {
+        if (CartItem.amount > 1) {
+          state.items[state.items.indexOf(CartItem)].amount -= 1;
+          state.totalCost -= action.payload.cost;
+        } else {
+          state.totalCost -= action.payload.cost;
+          state.items = state.items.filter(
+            (item) => action.payload.title !== item.title
+          );
         }
-    }
-})
+      }
+    },
+  },
+});
 
-export default cartItemsSlice.reducer
-export const {ADD} = cartItemsSlice.actions
+export default cartItemsSlice.reducer;
+export const { ADD, DELETE } = cartItemsSlice.actions;
